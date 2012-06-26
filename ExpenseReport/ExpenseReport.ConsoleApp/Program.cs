5c14963bc5c58b9b63b7371f0ef2b08a99a157ad
Michel Grootjans <michel.grootjans@gmail.com>
2012-06-26 16:56:57 +0200

    fixed a little

--- ExpenseReport/ExpenseReport.ConsoleApp/Program.cs
+++ ExpenseReport/ExpenseReport.ConsoleApp/Program.cs
@@ -8,8 +8,10 @@
             report.AddExpense(new Expense(ExpenseType.Breakfast, 525));
             report.AddExpense(new Expense(ExpenseType.Breakfast, 400));
             report.AddExpense(new Expense(ExpenseType.Breakfast, 500));
+            report.AddExpense(new Expense(ExpenseType.Breakfast, 1050));
             report.AddExpense(new Expense(ExpenseType.CarRental, 5600));
-            report.AddExpense(new Expense(ExpenseType.Dinner, 1400));
+            report.AddExpense(new Expense(ExpenseType.Dinner, 4800));
+            report.AddExpense(new Expense(ExpenseType.Dinner, 5400));
 
             report.PrintReport(new ConsoleReportPrinter());
         }
